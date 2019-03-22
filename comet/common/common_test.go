a080cf87c09fbfbb3f09555c57e02112519567ba
liyongsheng <liyongsheng@meicai.cn>
2019-03-22 18:54:58 +0800

    fix bug

--- comet/common/common_test.go
+++ comet/common/common_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestGetLocalIp(t *testing.T) {
     ip := GetLocalIp()
-    if ip==""{
+    if ip == "" {
         t.Error("获取本机ip错误")
     }
-    fmt.Println("本机ip: "+ip)
+    fmt.Println("本机ip: " + ip)
 }
