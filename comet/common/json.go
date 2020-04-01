ee41e2f0de760917d7d6d6292e737894bbf9f271
liyongsheng <liyongsheng@meicai.cn>
2020-04-01 23:02:33 +0800

    fix bug

--- comet/common/json.go
+++ comet/common/json.go
@@ -2,26 +2,20 @@ package common
 
 import "github.com/pquerna/ffjson/ffjson"
 
-/**
- * 序列化
- *
- * @param interface{} v
- * @return []byte, error
- */
+//EnJson 序列化
+//@param interface{} v
+//@return []byte, error
 func EnJson(v interface{}) ([]byte, error) {
-    return ffjson.Marshal(&v)
+	return ffjson.Marshal(&v)
 }
 
-/**
- * 反序列化
- *
- * @param []byte data
- * @param interface{} v
- * @return error
- */
+//DeJson 反序列化
+//@param []byte data
+//@param interface{} v
+//@return error
 func DeJson(data []byte, v interface{}) error {
-    if len(data) < 1 {
-        return nil
-    }
-    return ffjson.Unmarshal(data, v)
+	if len(data) < 1 {
+		return nil
+	}
+	return ffjson.Unmarshal(data, v)
 }
