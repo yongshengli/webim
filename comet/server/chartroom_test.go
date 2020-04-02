83fbb7d01b3ebe1fa6121d7a238880dd82af6b55
liyongsheng <liyongsheng@meicai.cn>
2020-04-02 08:52:49 +0800

    fix bug

--- comet/server/chartroom_test.go
+++ comet/server/chartroom_test.go
@@ -1,62 +1,63 @@
 package server
 
 import (
-    "testing"
-    "github.com/satori/go.uuid"
-    "webim/comet/common"
+	"testing"
+	"webim/comet/common"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestNewRoom(t *testing.T) {
-    common.RedisInitTest()
-    roomId := "123"
-    room, err := NewRoom(roomId, "")
-    if err != nil {
-        t.Error(err)
-    }
-    if room == nil {
-        t.Fail()
-    }
-    room, err = GetRoom(roomId)
-    if err != nil {
-        t.Error(err)
-    }
-    if room.Id == "" {
-        t.Fail()
-    }
-    _, err = room.Join(&Session{DeviceToken: uuid.NewV4().String(), IP: "127.0.0.1", User: &User{Id: "1", Name: "张三"}})
-    if err != nil {
-        t.Error(err)
-    }
-    users, err := room.Users()
-    if err != nil {
-        t.Error(err)
-    }
-    if len(users) < 1 {
-        t.Error("用户进入房间失败")
-    }
-    roomArr, err := RoomList(0, -1)
-    if err != nil {
-        t.Error(err)
-    }
-    if len(roomArr) < 1 {
-        t.Error("room zset 中没有数据")
-    }
-    roomNum, err := TotalRoom()
-    if err != nil {
-        t.Error(err)
-    }
-    if roomNum < 1 {
-        t.Error("房间数错误")
-    }
-    res, err := DelRoom(roomId)
-    if err != nil {
-        t.Error(err)
-    }
-    if res < 1 {
-        t.Error("删除聊天室失败")
-    }
-    room, err = GetRoom(roomId)
-    if err != nil {
-        t.Error(err)
-    }
+	common.RedisInitTest()
+	roomId := "123"
+	room, err := NewRoom(roomId, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if room == nil {
+		t.Fail()
+	}
+	room, err = GetRoom(roomId)
+	if err != nil {
+		t.Error(err)
+	}
+	if room.Id == "" {
+		t.Fail()
+	}
+	_, err = room.Join(&Session{DeviceToken: uuid.NewV4().String(), IP: "127.0.0.1", User: &User{Id: "1", Name: "张三"}})
+	if err != nil {
+		t.Error(err)
+	}
+	users, err := room.Users(0, -1)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(users) < 1 {
+		t.Error("用户进入房间失败")
+	}
+	roomArr, err := RoomList(0, -1)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(roomArr) < 1 {
+		t.Error("room zset 中没有数据")
+	}
+	roomNum, err := TotalRoom()
+	if err != nil {
+		t.Error(err)
+	}
+	if roomNum < 1 {
+		t.Error("房间数错误")
+	}
+	res, err := DelRoom(roomId)
+	if err != nil {
+		t.Error(err)
+	}
+	if res < 1 {
+		t.Error("删除聊天室失败")
+	}
+	room, err = GetRoom(roomId)
+	if err != nil {
+		t.Error(err)
+	}
 }
--- comet/server/server_context_test.go
+++ comet/server/server_context_test.go
@@ -1,37 +1,44 @@
 package server
 
 import (
-    "testing"
-    "fmt"
-    "webim/comet/common"
+	"fmt"
+	"testing"
+	"time"
+	"webim/comet/common"
+
+	"github.com/gomodule/redigo/redis"
 )
 
 func TestServerRegister(t *testing.T) {
-    server := Info{
-        Host : "127.0.0.1",
-        Port : "8000",
-    }
-    common.RedisInitTest()
-    context := new(Context)
+	server := Info{
+		Host:       "127.0.0.1",
+		Port:       "8000",
+		LastActive: time.Now().Unix(),
+	}
+	context := new(Context)
 
-    if _, err := context.Register(server.Host, server); err!=nil{
-        t.Error(err)
-    }
-    //fmt.Println(res)
-    sMap, err := context.List()
-    if err!= nil{
-        t.Error(err)
-    }
-    if len(sMap)<1{
-        t.Error("没有取到主机map")
-    }
-    fmt.Println(sMap)
-    context.Remove(server.Host)
-    serJson, err := common.RedisClient.Do("hget", serverMapKey(), server.Host)
-    if err != nil {
-        t.Error(err)
-    }
-    if serJson != nil {
-        t.Error("删除失败")
-    }
+	if _, err := context.Register(server); err != nil {
+		t.Error(err)
+	}
+	//fmt.Println(res)
+	list, err := context.List()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(list) < 1 {
+		t.Error("没有取到主机map")
+	}
+	fmt.Println(list)
+	context.Remove(server)
+	redisKey := fmt.Sprintf("%s:%s", server.Host, server.Port)
+	num, err := redis.Int64(common.RedisClient.Do("ZSCORE", serverMapKey(), redisKey))
+	if err != nil && err != redis.ErrNil {
+		t.Error(err)
+	}
+	if num > 0 {
+		t.Error("删除失败")
+	}
+}
+func init() {
+	common.RedisInitTest()
 }
