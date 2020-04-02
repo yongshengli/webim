6e970c3741d0c81d2b13bbe9fd5db46ac4aa9e86
liyongsheng <liyongsheng@meicai.cn>
2020-04-02 08:15:08 +0800

    fix bug

--- comet/server/server_context.go
+++ comet/server/server_context.go
@@ -3,76 +3,36 @@ package server
 import (
 	"fmt"
 	"net/rpc"
-	"net/rpc/jsonrpc"
 	"strings"
 	"time"
 	"webim/comet/common"
 
-	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/gomodule/redigo/redis"
 )
 
-const RPC_CHAN_NUM = 30
+type rpcClientPool chan *rpc.Client
 
-type rpcClinePool chan *rpc.Client
+//Context Server context
 type Context struct {
 	server           *server
-	serverRpcPoolMap map[string]rpcClinePool
-}
-
-func (sm *Context) CallRpcClient(host string, method, args string, reply interface{}) error {
-	if rpcPool, ok := sm.serverRpcPoolMap[host]; ok {
-		client := <-rpcPool
-		return client.Call(method, args, reply)
-	}
-	return nil
-}
-
-func (sm *Context) createRpcClinePool() {
-	serverMap, err := sm.List()
-	if err != nil {
-		logs.Error("msg[createRpcClientPool err] err[%s]", err.Error())
-	}
-	for _, s := range serverMap {
-		sm.addServerRpcClinePool(s.Host, s.Port)
-	}
-}
-
-func (sm *Context) addServerRpcClinePool(host, port string) bool {
-	if host == sm.server.Host {
-		return false
-	}
-	if _, ok := sm.serverRpcPoolMap[host]; ok {
-		return true
-	}
-	sm.serverRpcPoolMap[host] = make(chan *rpc.Client, RPC_CHAN_NUM)
-	addr := host + ":" + port
-	for i := 0; i < 10; i++ {
-		client, err := jsonrpc.Dial("tcp", addr)
-		if err != nil {
-			logs.Error("msg[newServerRpcChan err] err[%s]", err.Error())
-			continue
-		}
-		sm.serverRpcPoolMap[host] <- client
-	}
-	return true
+	serverRpcPoolMap map[string]rpcClientPool
 }
 
 //Register 注册server
 func (sm *Context) Register(s Info) (int, error) {
-	key := fmt.Sprintf("%s:%d", s.Host, s.Port)
+	key := fmt.Sprintf("%s:%s", s.Host, s.Port)
 	return redis.Int(common.RedisClient.Do("zadd", serverMapKey(), s.LastActive, key))
 }
 
 //List 列出全部server
 func (sm *Context) List() ([]Info, error) {
 	//"WITHSCORES"
-	serverArr, err := redis.Int64Map(common.RedisClient.Do("zrevrange", serverMapKey(), 0, -1))
+	serverArr, err := redis.Int64Map(common.RedisClient.Do("zrevrange", serverMapKey(), 0, -1, "WITHSCORES"))
 	if err != nil {
 		return nil, err
 	}
-
+	logs.Debug("msg[获取server列表] map[%v]", serverArr)
 	timeNow := time.Now().Unix()
 	res := make([]Info, 0)
 	for s, at := range serverArr {
@@ -92,13 +52,13 @@ func (sm *Context) List() ([]Info, error) {
 
 //Len 统计server个数
 func (sm *Context) Len() (int, error) {
-	return redis.Int(common.RedisClient.Do("HLEN", serverMapKey()))
+	return redis.Int(common.RedisClient.Do("zcard", serverMapKey()))
 }
 
 //Remove 移除server
 func (sm *Context) Remove(s Info) (int, error) {
-	key := fmt.Sprintf("%s:%d", s.Host, s.Port)
-	beego.Info("remove server " + key)
+	key := fmt.Sprintf("%s:%s", s.Host, s.Port)
+	logs.Info("remove server " + key)
 	return redis.Int(common.RedisClient.Do("zrem", serverMapKey(), key))
 }
 
--- comet/server/server_monitor.go
+++ comet/server/server_monitor.go
@@ -1,28 +1,37 @@
 package server
 
-import "github.com/astaxie/beego/logs"
+import (
+	"runtime"
 
+	"github.com/astaxie/beego/logs"
+)
+
+//StatusData 集群状态指标
 type StatusData struct {
-    UserNum    int `json:"user_num"`
-    SessionNum int `json:"conn_num"`
-    RoomNum    int `json:"room_num"`
-    ServerNum  int `json:"server_num"`
+	UserNum      int `json:"user_num"`
+	SessionNum   int `json:"conn_num"`
+	RoomNum      int `json:"room_num"`
+	ServerNum    int `json:"server_num"`
+	GoroutineNum int `json:"goroutine_num"`
 }
 
+//Status 集群服务状态
 func Status() StatusData {
-    monitor := StatusData{
-        UserNum:    0,
-        SessionNum: Server.CountSession(),
-    }
-    roomNum, err := TotalRoom()
-    if err != nil {
-        logs.Error("msg[获取room num err] err[%s]", err.Error())
-    }
-    monitor.RoomNum = roomNum
-    serverNum, err := Server.context.Len()
-    if err!=nil {
-        logs.Error("msg[获取server num err] err[%s]", err.Error())
-    }
-    monitor.ServerNum = serverNum
-    return monitor
+	monitor := StatusData{
+		UserNum:      0,
+		SessionNum:   Server.CountSession(),
+		GoroutineNum: runtime.NumGoroutine(),
+	}
+	roomNum, err := TotalRoom()
+	if err != nil {
+		logs.Error("msg[获取room num err] err[%s]", err.Error())
+	}
+	monitor.RoomNum = roomNum
+	serverNum, err := Server.context.Len()
+	if err != nil {
+		logs.Error("msg[获取server num err] err[%s]", err.Error())
+	}
+	monitor.ServerNum = serverNum
+
+	return monitor
 }
